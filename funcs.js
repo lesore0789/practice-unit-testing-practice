8cb8a1a8ca18280bc2325ba7f72972294c4e406f
Leslie <lesore0789@gmail.com>
2023-02-11 00:16:43 -0800

    Finished writing functions based on tests

--- funcs.js
+++ funcs.js
@@ -1,13 +1,25 @@
 function isFive(num) {
-  // Your code here
+  if(num == 5){
+    return true
+  }
+  return false
 }
 
 function isOdd(number) {
-  // Your code here
+  if(number % 2 == 0){
+    return false
+  } else if (typeof number !== 'number'){
+    throw Error(`${number} is not a number!`)
+  }
+  return true
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let array = [];
+  for(let i = min; i <= max; i += step){
+    array.push(i)
+  }
+  return array
 }
 
 
