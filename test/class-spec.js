205c58cc1def94660b349a37260230ebdc1fcfa0
Leslie <lesore0789@gmail.com>
2023-02-11 00:31:16 -0800

    Added test-specs for class

--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,38 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let orange;
+  this.beforeEach(() => {
+    orange = new Word("Orange")
+  })
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(orange).to.have.property("word");
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(orange.word).to.equal("Orange");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      orange.removeVowels();
+      expect(orange.removeVowels()).to.equal("rng");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      orange.removeConsonants();
+      expect(orange.removeConsonants()).to.equal("Oae");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      orange.pigLatin();
+      expect(orange.pigLatin()).to.equal("Orangeyay");
     });
   });
 });
